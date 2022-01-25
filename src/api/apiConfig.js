e1eb32ad9defae75f4ded5360ef74f4a92c46c20
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-01-25 22:54:54 +0600

    added env file

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "eed3abfec69ef1e08b863ec0a15d4fd0",
+  apiKey: process.env.REACT__APP__API__KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   img: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
